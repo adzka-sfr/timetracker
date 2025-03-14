7014cd90f9f97be660c59e984ac70f6e3871f00c
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-03-14 17:42:37 +0700

    update database

--- config.php
+++ config.php
@@ -2,19 +2,19 @@
 date_default_timezone_set('Asia/Jakarta');
 $now = date('Y-m-d H:i:s');
 
-$host = 'localhost';  // local
-$dbname = 'counter'; // local
-$username = 'root';  // local
-$password = '';  // local
+// $host = 'localhost';  // local
+// $dbname = 'counter'; // local
+// $username = 'root';  // local
+// $password = '';  // local
 
 $today = date('l', strtotime($now));
 $keygen = $today . '-vicidior';
 $keygen = strtolower($keygen);
 
-// $host = 'localhost';  // hosting
-// $dbname = 'u266480338_duit'; // hosting
-// $username = 'u266480338_bismillahadzka';  // hosting
-// $password = 'Alfianwai1';  // hosting
+$host = 'localhost';  // hosting
+$dbname = 'u266480338_counter'; // hosting
+$username = 'u266480338_adzkaganteng';  // hosting
+$password = 'Alfianwai1!';  // hosting
 
 try {
     $connect = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
